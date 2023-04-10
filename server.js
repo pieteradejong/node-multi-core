7b4f7e7c4d36a7b45c3ff7cf4ab498ae02b6dc65
Pieter Arthur de Jong <pieter.a.dejong@gmail.com>
2023-04-10 18:09:48 -0400

    perform simple async task per core

--- server.js
+++ server.js
@@ -6,12 +6,29 @@ const numberCPUs = require("os").cpus().length;
 
 console.log(`Process ${process.pid} started`);
 
+
+const main = async () => {
+    // Perform set of tasks async.
+    for(i = 1; i < 3; i++) {
+        console.log(`output ${i}`);
+    }
+}
+
 if(cluster.isMaster) {
-    console.log(`Number of CPUs is ${numberCPUs}`);
-    console.log(`Master process ${process.pid} is running`);
+    console.log(`[${process.pid}] Number of CPUs is ${numberCPUs}`);
+    console.log(`[${process.pid}] Master process is running`);
+
+    for(i = 0; i < numberCPUs; i++) {
+        cluster.fork();
+    }
 } else {
-    console.log(`Number of CPUs is ${numberCPUs}`);
-    console.log(`Worker process ${process.pid} is running`);
+    console.log(`[${process.pid}] Worker process is running`);
+    main()
+        .then(() => process.exit(0))
+        .catch((err) => {
+            console.log(`[${process.pid}] Caught error: ${err}`);
+            process.exit(1);
+    });
 }
 
 app.listen(PORT, () => {
